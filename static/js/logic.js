6cb9190fbb00726075ec9eec5cff3aab518b3642
derekdbates <derek.d.bates44@gmail.com>
2025-04-05 19:25:44 -0700

    created base and overlays

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,18 +1,48 @@
-// Create the 'basemap' tile layer that will be the background of our map.
+//geojson link https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
 
+// Create the 'basemap' tile layer that will be the background of our map.
+let basemap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 
 // OPTIONAL: Step 2
 // Create the 'street' tile layer as a second background of the map
-
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
 // Create the map object with center and zoom options.
+let map = L.map('map', {
+  center: [47.6061, -122.3328],
+  zoom: 6,
+  layers: [basemap, street]
+});
+
+
 
 
 // Then add the 'basemap' tile layer to the map.
+basemap.addTo(map);
 
 // OPTIONAL: Step 2
 // Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
+
+let layers = {
+  EARTHQUAKE: new L.LayerGroup(),
+  TECTONIC: new L.LayerGroup() 
+};
+
+let overlays = {
+  'Earthquake': layers.EARTHQUAKE,
+  'Tectonic Plates': layers.TECTONIC
+};
+
+
+
 // Add a control to the map that will allow the user to change which layers are visible.
+L.control.layers(null, overlays).addTo(map);
+
+
 
 
 // Make a request that retrieves the earthquake geoJSON data.
@@ -22,18 +52,23 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
+    color: getColor(feature.geometry[2]);
+    radius: getRadius(features.properties.mag);
+    fillopacity: .6,
+    weight: 1
 
-  }
+
+  };
 
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-
-  }
+    return features.geometry[2]
+  };
 
   // This function determines the radius of the earthquake marker based on its magnitude.
-  function getRadius(magnitude) {
-
-  }
+  function getRadius(mag) {
+    return Math.exp(features.properties.mag / 1.01 - .13)
+  };
 
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
